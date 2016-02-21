5366395ddd32d574e6b6f6b288093c3bac26cfd1
Gregg Franklin <greggfranklin@gmail.com>
2016-02-21 05:24:02 -0800

    Added Version Update Notification

--- mailbox-viewer.php
+++ mailbox-viewer.php
@@ -1,9 +1,11 @@
 <?php
 /**
-Plugin Name: Mailbox Viewer
-Plugin URL: https://serverpress.com/plugins/mail-proxy
-Description: Provides quick developer offline viewing of mail delivery services.
-Version: 1.0.0
-Author: Stephen Carnam
-Author URI: http://steveorevo.com
-*/
+ * Plugin Name: Mailbox Viewer
+ * Plugin URL: https://serverpress.com/plugins/mail-proxy
+ * Description: Provides quick developer offline viewing of mail delivery services.
+ * Version: 1.0.0
+ * Author: Stephen Carnam
+ * Author URI: http://steveorevo.com
+ *
+ * @package     DS3_Mailbox_Viewer
+ */
