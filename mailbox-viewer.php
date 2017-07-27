2de1b7a4018f0d808557324c3061d5b25d44d847
Dave Jesch <dave@davejesch.com>
2017-07-27 12:14:36 -0700

    update version numbers

--- mailbox-viewer.php
+++ mailbox-viewer.php
@@ -3,7 +3,7 @@
  * Plugin Name: Mailbox Viewer
  * Plugin URL: https://serverpress.com/plugins/mail-proxy
  * Description: Provides quick developer offline viewing of mail delivery services.
- * Version: 1.0.11
+ * Version: 1.0.12
  * Author: Stephen Carnam
  * Author URI: http://steveorevo.com
  *
